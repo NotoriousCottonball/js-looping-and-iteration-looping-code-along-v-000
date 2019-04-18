0828a426e74e88e330636cbb94e48dadf8d632e7
George  Pianka <gpianka1@gmail.com>
2019-04-18 06:29:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function printBadges(employeeNames) {
   for (let i = 0; i < employeeNames.length; i++) {
-    console.log(`Welcome ${empNames[i]}! You are employee #${i+1}.`);
+    console.log(`Welcome ${employeeNames[i]}! You are employee #${i+1}.`);
   }
   return employeeNames;
 }
