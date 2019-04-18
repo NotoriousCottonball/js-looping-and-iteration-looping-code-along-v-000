a9813c2be00957f29d723cfb888b3cc0f6462849
George  Pianka <gpianka1@gmail.com>
2019-04-18 06:29:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 
-function printBadges(empNames) {
-  for (let i = 0; i < empNames.length; i++) {
+function printBadges(employeeNames) {
+  for (let i = 0; i < employeeNames.length; i++) {
     console.log(`Welcome ${empNames[i]}! You are employee #${i+1}.`);
   }
   return empNames;
