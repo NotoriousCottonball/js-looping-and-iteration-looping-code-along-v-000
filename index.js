50af44d14420277829b2cc819910542ea7254132
George  Pianka <gpianka1@gmail.com>
2019-04-18 06:29:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function printBadges(employeeNames) {
   for (let i = 0; i < employeeNames.length; i++) {
     console.log(`Welcome ${empNames[i]}! You are employee #${i+1}.`);
   }
-  return empNames;
+  return employeeNames;
 }
 
  function tailsNeverFails() {
